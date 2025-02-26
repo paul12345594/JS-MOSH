de73de43c484c9da632f0be73a337cc07268da68
unknown <dungcapaul6@gmail.com>
2025-02-27 01:54:34 +0800

    4- PILLARS OF OOP

---
+++ MOSH/oop.js
@@ -0,0 +1,25 @@
+// let salary = 3000;
+// let overtime = 10;
+// let rate = 20;
+
+// function getWage (salary, overtime, rate){
+//     return salary + (overtime * rate);
+// }
+// console.log(getWage(salary,overtime,rate));
+
+
+//-----------------OBJECT-ORIENTED PROGRAMMING-------------
+//ENCAPSULATION 
+// let Employee = {
+//     salary: 3000,
+//     overtime: 10,
+//     rate: 20,
+
+//     netWage: function(){
+//         return this.salary + (this.overtime * this.rate);
+//     }
+// };
+// console.log(Employee.netWage())
+//-----------------------------------------------------------------------------------------------------
+
+
