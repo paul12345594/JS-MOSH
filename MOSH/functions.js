6309345f313caec79bd310df8deb11d8502ea1d3
unknown <dungcapaul6@gmail.com>
2025-02-27 00:20:25 +0800

    FUNCTIONS

---
+++ MOSH/functions.js
@@ -0,0 +1,14 @@
+
+//-----------------PERFOMING TASK       
+function greet(name, lastName) {
+    console.log('HELLO BOSS ' + name + '' + lastName);
+}
+
+//-----------------CALCULATING A VALUE
+
+function square(number){
+    return number + number ;
+}
+// let number = square(2)
+console.log(square(3))
+
