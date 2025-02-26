2de5953acc417d8f69ae5554b24d68d69cba7ef0
unknown <dungcapaul6@gmail.com>
2025-02-26 23:33:18 +0800

    DATA TYPES | OBJECT - DOT / BRACKET NOTATION | ARRAY

---
+++ MOSH/Dtypes.js
@@ -0,0 +1,38 @@
+//  let name = "paulworx";
+//  console.log(name);
+//RULES
+// Cannot be reserved keyword
+// Should be meaningful 
+// Cannot start with a number
+// Cannot contain a space or hypen (-)
+// Are case-sensitive (UNIQUE)
+
+
+// //Data types 
+// let Name = 'Mosh';
+// let age = 30;
+// let isApproved = false;
+// let firstName = undefined;
+// let selectedColor = null;   // OBJECT
+
+
+// OBJECTS
+// let person = {
+//     name: "paulallen",
+//     age: 22,
+// }
+
+// // // //Dot - Notation
+// person.name = " AngelCyril"
+// console.log(person.name);
+
+// // // //Bracket Notation []
+// let akoto = 'name';
+// person[akoto] = 'Mary';
+// console.log(person["name"]);
+
+
+//ARRAY 
+let selectedColor = ['red', 2, 'blue', 'PAULWORX', ];
+selectedColor[3] = ['SILVER'];   // FOR REPLACE
+console.log(selectedColor);
