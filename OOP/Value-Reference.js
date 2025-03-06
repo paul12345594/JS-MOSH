1302fad2afa510b6255e10d4c0e25baaa14f5428
unknown <dungcapaul6@gmail.com>
2025-03-06 23:30:52 +0800

    ENUMERATING PROPERTIE

--- OOP/Value-Reference.js
+++ OOP/Value-Reference.js
@@ -7,17 +7,35 @@
 // Object
 
 //Adding and Removing Properties 
-function Circle (radius){
+//  
+
+
+// Enumerating Properties
+function Circle (radius){                   // PROCESS
     this. radius = radius;
     this.draw = function () {
         console.log('draw');
     }
 }
 const circle = new Circle (10);
-circle.mocessss = {x:1};
 
-// delete the Property 
-delete circle.mocessss;
+for (let key in circle) {                   // to enumerate all members and an object use FOR loop 
+    if (typeof circle[key] !== 'function')
+    console.log(key, circle[key])
+}
+
+const keys = Object.keys(circle);           // to get all OBJECT keys 
+console.log(keys);
+
+if ('radius' in circle)                     // check for exixtence of property or method in object use IN OPERATOR
+    console.log('circle has a radius')
+
+
+
+
+
+
+
 
 
 
